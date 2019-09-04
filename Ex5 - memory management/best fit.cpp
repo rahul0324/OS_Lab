cbd7a0e9799312ee88e715d44b01ebb0bea5c175
Rahul Kumar <rahul.kr0324@gmail.com>
2019-09-05 01:14:46 +0530

    Add files via upload

---
+++ Ex5 - memory management/best fit.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int n,m;
+	cout<<"ENTER NUMBER OF PROCESSES AND NUMBER OF BLOCKS\n";
+	cin>>n>>m;
+	int process[n];
+	int blocks[m];
+	int assigned[n];
+	bool done[m];
+
+	cout<<"ENTER THE PROCESS SIZES:\n";
+	for(int i=0 ;i <n; i++)
+		cin>>process[i];	
+
+cout<<"ENTER THE BLOCK SIZES:\n";
+	for(int i=0 ;i <m; i++)
+		cin>>blocks[i];	
+
+	for(int i=0 ;i <n; i++)
+		assigned[i] = -1;
+	for(int i=0 ;i <m; i++)
+		done[i] = false;
+
+	for(int i=0;i<n ; i++){			//loop for processes
+		int j,maxx = INT_MAX,index =-1;
+		for( j=0; j<m; j++){			//loop for blocks
+			// if(process[i] <= blocks[j] && done[j] == false){
+			// 	blocks[j] -=  process[i];
+			// 	assigned[i] = j+1;
+			// 	done[j] = true;
+			// 	break;
+			// }
+			if(done[j] == false && process[i] <= blocks[j]){
+				if(blocks[j]-process[i] < maxx){
+
+				maxx = blocks[j]-process[i] ;
+				index = j;
+				}
+			}
+		} 
+		if(index !=-1){
+		assigned[i] = index+1;
+		done[index] = true;
+		}
+ 
+	}
+
+	cout<<"FINAL ASSIGNMENT IS \n";
+	for(int i=0;i<n;i++){
+		if(assigned[i] != -1)
+		cout<<"process no - "<<process[i]<<" is assigned to "<<assigned[i]<<endl;
+	else
+		cout<<"process no - "<<process[i]<<" is not assigned \n"<<endl;
+
+	}
+
+}
+
+/*
+input:
+4 5
+212 417 112 426
+100 500 200 300 600
+*/
